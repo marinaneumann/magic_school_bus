583ea3a26c49c5413d5caeedb4e6064837684369
Scott Dudley & Marina Neumann <student@epicodus.com>
2014-09-08 10:56:08 -0700

    Validate uniqueness of station name.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,4 @@
 class Station < ActiveRecord::Base
   validates :name, :presence => true
+  validates_uniqueness_of :name
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Station do
   it{ should validate_presence_of :name}
+  it{ should validate_uniqueness_of :name}
 end
