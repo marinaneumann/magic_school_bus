97e99c67449cb30a4e2428683680ce4718e384e2
Scott Dudley & Marina Neumann <student@epicodus.com>
2014-09-08 11:17:20 -0700

    Establish methods for station controller.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,3 +1,42 @@
 class StationsController <ApplicationController
+  def index
+    @stations = Station.all
+  end
+
+  def new
+    @station = Station.new
+  end
+
+  def create
+    @station = Station.new(params[:station])
+    if @station.save
+      redirect_to stations_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @station = Station.find(params[:id])
+  end
+
+  def edit
+    @station = Station.find(params[:id])
+  end
+
+  def update
+    @station = Station.find(params[:id])
+    if @station.update(params[:station])
+      redirect_to station_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @station = Station.find(params[:id])
+    @station.destroy
+    redirect_to stations_path
+  end
 
 end
